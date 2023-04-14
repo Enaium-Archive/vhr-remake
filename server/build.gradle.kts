4a8623d945c864e318ecdb404540b2c44d74cbd8
Enaium <wyy-666@hotmail.com>
2023-04-14 18:58:38 +0800

    dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.spring") version "1.8.20"
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("kapt")
+    id("com.google.devtools.ksp")
 }
 
 group = "cn.enaium"
@@ -15,7 +17,16 @@ repositories {
     mavenCentral()
 }
 
+val jimmer : String by project
+val mapstruct : String by project
+
 dependencies {
+    implementation("org.babyfish.jimmer:jimmer-spring-boot-starter:${jimmer}")
+    ksp("org.babyfish.jimmer:jimmer-ksp:$jimmer")
+    implementation("org.mapstruct:mapstruct:${mapstruct}")
+    kapt("org.mapstruct:mapstruct-processor:${mapstruct}")
+    kapt("org.babyfish.jimmer:jimmer-mapstruct-apt:${jimmer}")
+
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
--- server/settings.gradle.kts
+++ server/settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "vhr-remake"
+
+pluginManagement {
+    val kotlin: String by settings
+    repositories {
+        gradlePluginPortal()
+    }
+
+    plugins {
+        kotlin("jvm") version kotlin
+        kotlin("plugin.spring") version kotlin
+        kotlin("kapt") version kotlin
+        id("com.google.devtools.ksp") version "${kotlin}-1.0.9"
+    }
+}
